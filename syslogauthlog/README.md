59d10c15e6a2e927f7954708f5badcdf045b2b66
Markus Neubauer <neubauer@email-online.org>
2014-12-28 07:25:43 +0100

    moved to joomlatic

---
+++ syslogauthlog/README.md
@@ -0,0 +1,77 @@
+syslogauthlog
+=============
+
+# Joomla! - System - Syslog AuthLog - Plugin
+
+A Syslog authentication logger plugin for Joomla 2.5 and 3, which logs selected authentication events to AUTH log.
+
+## Logged Events
+- User login
+- User login failure
+- User logout
+- User logout failure
+
+## Which data is logged?
+System syslog sets:
+- Date
+- Time
+
+Plugin logs:
+- Severity
+- System user (for virtual environments)
+- Event
+- Username
+- Details (if any)
+- User IP
+
+## Features
+Configurable options:
+- log mode
+  - succesful and failed
+  - failed only
+- log event
+  - all events
+  - only login
+  - only logout
+- log environment
+  - front- and backend
+  - backend only
+
+## Installation
+Upload/Install the syslogauthlog plugin package and activate it in the Plug-in Manager. Select your logging options.
+The log entries will appear in your systems auth log directory (depending on distribution).
+
+## (unix) syslog
+
+Format: 'Dec 27 13:14:14 host jauthlog[PID]: [SYSUSER] {PRIORITY} {EVENT} {USERNAME} {ADMIN} {MESSAGE} from {CLIENTIP}'
+
+Syslog facility: LOG_AUTH
+
+Syslog priority:
+- LOG_INFO ~ Normal log entry
+- LOG_WARNING ~ Failure conditions
+
+## Special configuration
+
+Fail2ban can react on error conditions if you configure a block and a filter.
+
+For debian/ubuntu systems in jail.local:
+```ini
+…
+ [joomla-admin]
+ enabled  = true
+ port     = http,ftp
+ filter   = joomla-admin
+ logpath  = /var/log/auth.log
+ maxretry = 2
+ bantime  = 84600
+ findtime = 21150
+…
+```
+
+in filter.d/joomla-admin.conf
+```ini
+ [Definition]
+ failregex =  .* WARNING login admin ADMIN Benutzername und Passwort falsch .* from <HOST>$
+```
+Adapt the message text 'Benutzername und Passwort falsch' to your own language before loading the jail.
---
+++ syslogauthlog/src/syslogauthlog.php
@@ -0,0 +1,153 @@
+<?php
+/*
+ * @copyright  Copyright (C) 2014 Markus Neubauer. All rights reserved.
+ * @license    http://www.gnu.org/licenses/agpl-3.0.html GNU/AGPL
+ * @github     https://github.com/marneu/syslogauthlog
+ * @homepage   http://www.std-soft.com/index.php/hm-service/81-c-std-service-code/9-joomla-plugin-syslogauthlog
+ */
+
+defined('_JEXEC') or die();
+
+
+class PlgSystemSyslogAuthLog extends JPlugin {
+	
+	private $format = '{PRIORITY} {EVENT} {USERNAME} {ADMIN} {MESSAGE} from {CLIENTIP}';
+	private $syslog_options = array(
+				'sys_ident' => 'jauthlog',
+				'sys_add_pid' => true,
+				'sys_use_stderr' => false,
+				'sys_facility' => LOG_AUTH,
+				);
+	private $ignore_ip = array (
+				array('0.0.0.0','2.255.255.255'),
+				array('10.0.0.0','10.255.255.255'),
+				array('127.0.0.0','127.255.255.255'),
+				array('169.254.0.0','169.254.255.255'),
+				array('172.16.0.0','172.31.255.255'),
+				array('192.0.2.0','192.0.2.255'),
+				array('192.168.0.0','192.168.255.255'),
+				array('255.255.255.0','255.255.255.255')
+			      );
+	private $field = array();
+	private $message_option = array();
+	private $log_priority = array(
+					JLog::WARNING => 'WARNING',
+					JLog::INFO => 'INFO'
+				);
+
+
+	private function check_ip($ip)
+	{
+ 		if (!empty($ip) && ip2long($ip) != -1) {
+			foreach ($this->ignore_ip as $r) {
+				$min = ip2long($r[0]);
+				$max = ip2long($r[1]);
+				if ((ip2long($ip) >= $min) && (ip2long($ip) <= $max)) return false;
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+
+	private function getAddr()
+	{
+		if ($this->check_ip($_SERVER["HTTP_CLIENT_IP"])) {
+			return $_SERVER["HTTP_CLIENT_IP"];
+		}
+		foreach (explode(",",$_SERVER["HTTP_X_FORWARDED_FOR"]) as $ip) {
+			if ($this->check_ip(trim($ip))) {
+				return $ip;
+			}
+		}
+		if ($this->check_ip($_SERVER["HTTP_X_FORWARDED"])) {
+			return $_SERVER["HTTP_X_FORWARDED"];
+		} elseif ($this->check_ip($_SERVER["HTTP_FORWARDED_FOR"])) {
+			return $_SERVER["HTTP_FORWARDED_FOR"];
+		} elseif ($this->check_ip($_SERVER["HTTP_FORWARDED"])) {
+			return $_SERVER["HTTP_FORWARDED"];
+		} else {
+			return $_SERVER["REMOTE_ADDR"];
+		}
+	}
+
+
+	private function log() {
+
+		if (JFactory::getApplication()->isAdmin()) { 
+			$this->field['ADMIN'] = 'ADMIN';
+		}
+		else {
+			if ( $this->params->def('type', 1) == 1 ) return;
+			$this->field['ADMIN'] = '';
+		}
+
+		$this->message_option['category'] = getenv('USER');
+
+		$this->field['CLIENTIP'] = $this->getAddr();
+		
+		$this->field['PRIORITY'] = $this->log_priority[$this->message_option['priority']];
+
+		// Fill in field data for the line.
+		$message = $this->format;
+		if (!isset($this->field['MESSAGE'])) $this->field['MESSAGE'] = '';
+
+		foreach ($this->field as $tmp => $val)
+		{
+			$message = str_replace('{' . $tmp . '}', $val, $message);
+		}
+
+		$jLogEntry = new JLogEntry($message, $this->message_option['priority'], $this->message_option['category']);
+
+		// Write the new entry to syslog.
+		$syslog = new JLogLoggerSyslog($this->syslog_options);
+		$syslog::addEntry($jLogEntry);
+
+	}
+
+	public function onUserAfterLogin($options)
+	{ // JAuthenticationResponse array
+
+		if ( $this->params->def('mode', 1) != 0 ) return;
+		if ( $this->params->def('event', 1) == 2 ) return;
+		$this->message_option['priority'] = JLog::INFO;
+		$this->field['EVENT'] = 'login';
+		$this->field['USERNAME'] = $options['user']->username;
+		$this->log();
+	}
+
+	public function onUserLoginFailure($response) 
+	{
+
+		if ( $this->params->def('event', 1) == 2 ) return;
+		$this->message_option['priority'] = JLog::WARNING;
+		$this->field['EVENT'] = 'login';
+		$this->field['USERNAME'] = $response['username'];
+		$this->field['MESSAGE'] = $response['error_message'];
+		$this->log();
+	}
+
+	public function onUserAfterLogout($options)
+	{
+
+		if ( $this->params->def('mode', 1) != 0 ) return;
+		if ( $this->params->def('event', 1) == 1 ) return;
+		$this->message_option['priority'] = JLog::INFO;
+		$this->field['EVENT'] = 'logout';
+		$this->field['USERNAME'] = $options['username'];
+		$this->log();
+	}
+
+	public function onUserLogoutFailure($parameters)
+	{
+
+		if ( $this->params->def('event', 1) == 1 ) return;
+		$this->message_option['priority'] = JLog::WARNING;
+		$this->field['EVENT'] = 'logout';
+		$this->field['USERNAME'] = $parameters['username'];
+		$this->field['MESSAGE'] = 'failure';
+		$this->log();
+	}
+
+}
